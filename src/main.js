05bef1a193bd3f3ee966541a7c3e91f6e0b36de6
Dmitrii <sushkovd98@mail.ru>
2021-02-03 11:40:24 +0100

    date creation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import dateFilter from '@/filters/date.filter.js'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.filter('dateFilter', dateFilter) // название , функция
 
 new Vue({
   router,
