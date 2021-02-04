34391f3fc8347787b81203d753fa1a9e1066c565
Dmitrii <sushkovd98@mail.ru>
2021-02-04 18:04:28 +0100

    vuelidate-register

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
+import messagePlugin from '@/utils/message.plugin'
 import dateFilter from '@/filters/date.filter.js'
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate)
+Vue.use(messagePlugin)
+
 Vue.filter('dateFilter', dateFilter) // название , функция
 
 new Vue({
